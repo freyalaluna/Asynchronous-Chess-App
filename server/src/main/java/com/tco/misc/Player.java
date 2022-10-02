fd1abe1af458adc11136b973737f14298431ec78
Rhea Chan <rheachan9983@gmail.com>
2022-10-02 14:58:06 -0600

    added player class skeleton

---
+++ server/src/main/java/com/tco/misc/Player.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+//import Account.java;
+
+public class Player {
+    public boolean color;
+    public ArrayList<String> capturedPieces; // TODO: needs to be ArrayList<Pieces>
+    public ArrayList<String> remainingPieces; // TODO: needs to be ArrayList<Pieces>
+    private int accountID;
+    public String username;
+    
+    public int getAccountID(){
+        return 0;
+    }
+
+    public String getUsername(){
+        return "Username";
+    }
+    
+    
+}
