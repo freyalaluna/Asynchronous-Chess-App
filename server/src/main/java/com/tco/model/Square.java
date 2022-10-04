81d5874d3f986172569ec631f0f1fbbdecf261de
Victor Berggren <victorberggren3@gmail.com>
2022-10-03 23:06:49 -0600

    added square class

---
+++ server/src/main/java/com/tco/model/Square.java
@@ -0,0 +1,21 @@
+package com.tco.model;
+
+import java.util.ArrayList;
+
+public class Square {
+  private int x;
+  private int y;
+
+  public Square(int xCoord, int yCoord) {
+    x = xCoord;
+    y = yCoord;
+  }
+
+  public void getX() {
+    return x;
+  }
+
+  public void getY() {
+    return y;
+  }
+}
