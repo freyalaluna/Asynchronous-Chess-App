0951c83cf1df0c8acb3dec5ff9389380a8ee25be
Cory Raetz <raetz.cory@gmail.com>
2022-10-03 19:11:46 -0600

    added skeleton MatchRecord class

---
+++ server/src/main/java/com/tco/misc/MatchRecord.java
@@ -0,0 +1,25 @@
+package com.tco.misc;
+
+public class MatchRecord {
+   private MoveLog moveLog;
+   private Player winner;
+   private Player loser;
+   
+   public MatchRecord(MoveLog moveLog, Player winner, Player loser){
+        this.moveLog = moveLog;
+        this.winner = winner;
+        this.loser = loser;
+   }
+
+   public Player getWinner(){
+    return winner;
+   }
+
+   public Player getLoser(){
+    return loser;
+   }
+
+   public String displayMoveLog(){
+    return "";
+   }
+}
