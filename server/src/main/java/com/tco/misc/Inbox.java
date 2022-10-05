ecf251f6bfd165fedf01d29f6b28078a875ffa78
Freya LaLuna <freya.laluna@colostate.edu>
2022-10-04 22:39:59 -0600

    Created Inbox class skeleton

---
+++ server/src/main/java/com/tco/misc/Inbox.java
@@ -0,0 +1,28 @@
+package account_features;
+import java.util.ArrayList;
+
+public class Inbox {
+    ArrayList<Notification> allNotifs;
+    ArrayList<Invitation> allInvites;
+
+    public Inbox(){
+        allNotifs = new ArrayList<Notification>();
+        allInvites = new ArrayList<Invitation>();
+    }
+
+    public void addInvite(Invitation newInvite){
+        //TODO: implement this
+    }
+
+    public void addNotification(Notification newNotif){
+        //TODO: implement this
+    }
+
+    public ArrayList<Notification> getNotificationList(){
+        return allNotifs;
+    }
+
+    public ArrayList<Invitation> getInvitationList(){
+        return allInvites;
+    }
+}
