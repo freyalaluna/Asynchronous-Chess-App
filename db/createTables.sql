8deabfc550191148fb3354ea4c3f081d43b0d061
Victor Berggren <victorberggren3@gmail.com>
2022-10-09 12:05:17 -0600

    added table

--- db/createTables.sql
+++ db/createTables.sql
@@ -5,14 +5,22 @@
 -- CREATE TABLE user comes before this
 CREATE TABLE notifications (
     notificationID int PRIMARY KEY,
-    recipientID int NOT NULL.
+    recipientID int NOT NULL,
     senderID int NOT NULL,
     messageType String NOT NULL,
     FOREIGN KEY (recipientID) REFERENCES user(userID),
     FOREIGN KEY (senderID) REFERENCES user(userID)
 );
 
-CREATE TABLE matchRecordTable(
+CREATE TABLE ongoingMatch (
+    matchID int PRIMARY KEY,
+    playerTurn boolean NOT NULL,
+    gameStateFEN text,
+    FOREIGN KEY (whitePlayer) REFERENCES user(userID),
+    FOREIGN KEY (blackPlayer) REFERENCES user(userID)
+);
+
+CREATE TABLE matchRecordTable (
     userID int NOT NULL,
     matchID int NOT NULL,
     beginTime varchar(30) NOT NULL,
