2a2a824894d4e23f6774fe7e9cbacfaa58708661
Cory Raetz <raetz.cory@gmail.com>
2022-10-09 16:58:03 -0600

    added bishop test cases

--- server/src/main/java/com/tco/model/Bishop.java
+++ server/src/main/java/com/tco/model/Bishop.java
@@ -14,10 +14,11 @@ public class Bishop extends Piece {
     int currentX = currentPosition.getX();
     int currentY = currentPosition.getY();
     // up left
-    for(int i = currentPosition.getX()-1; i >= 0; i--){
+    for(int i = 0; i < 8; i++){
       currentX--;
       currentY--;
-      if(boardState[currentX][currentY] != 'o'){
+      if(!isValidCoord(currentX,currentY)) {break;}
+      else if(boardState[currentX][currentY] != 'o'){
         if(Character.isUpperCase(boardState[currentX][currentY])== color){
         }
         else{
@@ -32,10 +33,11 @@ public class Bishop extends Piece {
     currentX = currentPosition.getX();
     currentY = currentPosition.getY();
     // up right
-    for(int i = currentPosition.getX(); i<=7 ; i++){
+    for(int i = 0; i < 8; i++){
       currentX++;
       currentY--;
-      if(boardState[currentX][currentY] != 'o'){
+      if(!isValidCoord(currentX,currentY)) {break;}
+      else if(boardState[currentX][currentY] != 'o'){
         if(Character.isUpperCase(boardState[currentX][currentY])== color){
         }
         else{
@@ -50,10 +52,11 @@ public class Bishop extends Piece {
     currentX = currentPosition.getX();
     currentY = currentPosition.getY();
     // down left
-    for(int i = currentPosition.getY(); i <= 7; i++){
+    for(int i = 0; i < 8; i++){
       currentY++;
       currentX--;
-      if(boardState[currentX][currentY] != 'o'){
+      if(!isValidCoord(currentX,currentY)) {break;}
+      else if(boardState[currentX][currentY] != 'o'){
         if(Character.isUpperCase(boardState[currentX][currentY])== color){
         }
         else{
@@ -68,10 +71,11 @@ public class Bishop extends Piece {
     currentX = currentPosition.getX();
     currentY = currentPosition.getY();
     //down right
-    for(int i = currentPosition.getY(); i <= 7; i++){
+    for(int i = 0; i < 8; i++){
       currentX++;
       currentY++;
-      if(boardState[currentX][currentY] != 'o'){
+      if(!isValidCoord(currentX,currentY)) {break;}
+      else if(boardState[currentX][currentY] != 'o'){
         if(Character.isUpperCase(boardState[currentX][currentY])== color){
         }
         else{
@@ -82,6 +86,12 @@ public class Bishop extends Piece {
         possibleMoves.add(new Square(currentX,currentY));
       }   
     }
+
     return possibleMoves;
+  }
+
+  private boolean isValidCoord(int x, int y) {
+    if (x>=0 && x<8 && y>=0 && y<8) return true;
+    return false;
   }
 }
---
+++ server/src/test/java/com/tco/model/TestBishop.java
@@ -0,0 +1,54 @@
+package com.tco.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+
+public class TestBishop {
+    
+    private static Board board = new Board();
+    private static char[][] boardState;
+
+    @BeforeEach
+    public void resetBoardState() {
+        boardState = board.getBoardState();
+    }
+
+    @Test
+    @DisplayName("craetz: test getPossibleMoves for left white bishop in starting position")
+    public void testValidMovesWhiteBishopC1(){
+        Bishop bishop = new Bishop(true, new Square(7,2));
+        ArrayList<Square> result = bishop.getPossibleMoves(boardState);
+        ArrayList<Square> expected = new ArrayList<Square>();
+        assertEquals(expected,result);
+    }
+
+    @Test
+    @DisplayName("craetz: test getPossibleMoves for left black bishop in starting position")
+    public void testValidMovesBlackBishopC8(){
+        Bishop bishop = new Bishop(false,new Square(0,2));
+        ArrayList<Square> result = bishop.getPossibleMoves(boardState);
+        ArrayList<Square> expected = new ArrayList<Square>();
+        assertEquals(expected,result);
+    }
+
+    @Test
+    @DisplayName("craetz: test getPossibleMoves for white bishop mid-board")
+    public void testValidMovesWhiteBishopF4()
+    {
+        Bishop bishop = new Bishop(true, new Square(4,5));
+        ArrayList<Square> result = bishop.getPossibleMoves(boardState);
+        ArrayList<Square> expected = new ArrayList<Square>();
+        expected.add(new Square(2,3));
+        expected.add(new Square(3,4));
+        expected.add(new Square(2,7));
+        expected.add(new Square(3,6));
+        expected.add(new Square(5,4));
+        expected.add(new Square(5,6));
+    } 
+}
