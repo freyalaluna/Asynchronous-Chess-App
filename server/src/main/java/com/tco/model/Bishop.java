022ceaca2a64b11729b49ac5909ffbe8e6c36a16
Cory Raetz <raetz.cory@gmail.com>
2022-10-09 15:01:11 -0600

    implement getPossibleMoves for Bishop

--- server/src/main/java/com/tco/model/Bishop.java
+++ server/src/main/java/com/tco/model/Bishop.java
@@ -11,6 +11,65 @@ public class Bishop extends Piece {
   public ArrayList<Square> getPossibleMoves(char[][] boardState) {
     ArrayList<Square> possibleMoves = new ArrayList<>();
     //TODO implement this
+    int currentX = currentPosition.getX();
+    int currentY = currentPosition.getY();
+
+    // up left
+    for(int i = currentPosition.getX()-1; i >= 0; i--)
+    {
+      currentX--;
+      currentY--;
+      possibleMoves.add(new Square(currentX,currentY));
+
+      if(boardState[currentX][currentY] != 'o')
+        break;
+    }
+
+    currentX = currentPosition.getX();
+    currentY = currentPosition.getY();
+
+    // up right
+    for(int i = currentPosition.getX(); i<=7 ; i++)
+    {
+      currentX++;
+      currentY--;
+
+      possibleMoves.add(new Square(currentX,currentY));
+
+      if(boardState[currentX][currentY] != 'o')
+        break;
+
+    }
+
+    currentX = currentPosition.getX();
+    currentY = currentPosition.getY();
+    // down left
+    for(int i = currentPosition.getY(); i <= 7; i++)
+    {
+      currentY++;
+      currentX--;
+
+      possibleMoves.add(new Square(currentX,currentY));
+
+      if(boardState[currentX][currentY] != 'o')
+        break;
+
+    }
+
+    currentX = currentPosition.getX();
+    currentY = currentPosition.getY();
+
+    //down right
+    for(int i = currentPosition.getY(); i <= 7; i++)
+    {
+      currentX++;
+      currentY++;
+
+      possibleMoves.add(new Square(currentX,currentY));
+
+      if(boardState[currentX][currentY] != 'o')
+        break;
+    }
 
     return possibleMoves;
   }
