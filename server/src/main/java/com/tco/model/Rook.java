0072e0b15be8ea693c3f4bc8ebff23b2852883be
Rhea Chan <rheachan9983@gmail.com>
2022-10-11 22:33:23 -0600

    add possible moves to right of rook current position

--- server/src/main/java/com/tco/model/Rook.java
+++ server/src/main/java/com/tco/model/Rook.java
@@ -10,8 +10,30 @@ public class Rook extends Piece {
 
   public ArrayList<Square> getPossibleMoves(char[][] boardState) {
     ArrayList<Square> possibleMoves = new ArrayList<>();
-    //TODO implement this
-
+    possibleMoves.add(getMovesToRight(currentPosition, boardState));
     return possibleMoves;
+  }
+  public ArrayList<Square> getMovesToRight(Square initialPosition, char[][] boardState) {
+    ArrayList<Square> getRightDirectionMoves = new ArrayList<>();
+    int currentRow = initialPosition.getX();
+    int currentColumn = initialPosition.getY();
+    while(isValidCoord(currentRow, currentColumn)){
+      currentColumn++;
+      if (Character.isUpperCase(boardState[currentRow][currentColumn])==color) {
+        currentColumn++;
+        if(!isValidCoord(currentRow, currentColumn)){
+          break;
+        }
+        else{
+          getRightDirectionMoves.add(new Square(currentRow, currentColumn));
+        }
+      }
+    return getRightDirectionMoves;
+
+  }
+
+  private boolean isValidCoord(int x, int y) {
+    if (x>=0 && x<8 && y>=0 && y<8) return true;
+    return false;
   }
 }
