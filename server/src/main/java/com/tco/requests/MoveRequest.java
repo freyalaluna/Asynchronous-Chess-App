b007f45020b8543d8378f2c9b71c3151518cefdc
Victor Berggren <victorberggren3@gmail.com>
2022-10-26 10:24:18 -0600

    added test class

--- server/src/main/java/com/tco/requests/MoveRequest.java
+++ server/src/main/java/com/tco/requests/MoveRequest.java
@@ -1,5 +1,12 @@
 package com.tco.requests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MoveRequest {
+  private static final transient Logger log = LoggerFactory.getLogger(MoveRequest.class);
   
+  @Override
+  public void buildResponse() {
+  }
 }
--- server/src/test/java/com/tco/requests/TestMoveRequest.java
+++ server/src/test/java/com/tco/requests/TestMoveRequest.java
@@ -1,5 +1,20 @@
 package com.tco.requests;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
 public class TestMoveRequest {
-  
+  private MoveRequest req;
+
+  @BeforeEach
+  public void createRequestForTestCases() {
+    req = new MoveRequest();
+  }
+
+  @Test
+  @DisplayName("victor45: initial state")
+  public void testInitialState() {
+    
+  }
 }
