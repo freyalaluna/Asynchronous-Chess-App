1818708a81ca5c337b3316b83f4dae6199e91fda
Freya LaLuna <freya.laluna@colostate.edu>
2022-10-26 21:51:43 -0600

    Added API calls, PW encryption, Header to Signup and Login, modified form centering on login.css

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,11 +1,14 @@
-import React, {useEffect, useRef, useState} from 'react';
-import '../static/styles/login.css'
-//import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
+import React, {useEffect, useState} from 'react';
+import Header from './Header/Header';
+import { sendAPIRequest, getOriginalServerUrl } from '../utils/restfulAPI';
+//import {sendAccountRequest} from '../hooks/useAccount';
+import '../static/styles/login.css';
+//import { Link, Redirect } from 'react-router-dom'; 
 
-//const Email_Scheme = /^[a-zA-Z0-9]{1,}@[a-zA-Z]{1,}/;
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
 
 const Signup = () => {
-    const errorRef = useRef();
 
     const [formData, setFormData] = useState({
         email: "",
@@ -13,30 +16,33 @@ const Signup = () => {
         password: ""
     })
 
+    const [confPassword, setConfPassword] = useState('');
+
     const [errorMessage, setErrorMessage] = useState('');
-    const [success, setSuccess] = useState(false);
 
-    /*const submitSignin = async (e) => {
-        e.preventDefault();
-        if(!Email_Scheme.test(email)){
-            setErrorMessage('Email must be of form \'name@domain.extension\'');
-            return
+    const validateEmail = (str = "") => {
+        if(!str.includes("@")){
+            setErrorMessage(errorMessage + 'Email must be of form \'name@domain.extension\'\n')
+            return false
         }
-        console.log(user, email, password);
-        const response = await sendAPIRequest({
-            method: 'GET'
-            data: email, username
-        }, getOriginalServerURL);
-        //Fetch data from API call in try/catch
-        //If email or username are already in db, set errorMessage accordingly
-        //Otherwise, display success and make an API call to insert new user data into db
-        
-    }*/
+        return true
+    }
 
-    const handleSignin = e => {
-        e.preventDefault();
-        console.log(formData);
-    };
+    const validateUsername = (str = "") => {
+        if(str.length === 0){
+            setErrorMessage(errorMessage+'Username field must be filled out\n');
+            return false;
+        }
+        return true;
+    }
+
+    const validatePass = (pass, confirmPass) => {
+        if(pass !== confirmPass){
+            setErrorMessage(errorMessage+'Passwords must match\n');
+            return false;
+        }
+        return true;
+    }
 
     const handleChange = e => {
         setFormData({
@@ -45,19 +51,51 @@ const Signup = () => {
         })
     };
 
+    const handlePWChange = e => {
+        setConfPassword(e.target.value);
+    }
+
+    const handleSignin = async(e) => {
+        e.preventDefault();
+        setErrorMessage("");
+
+        //This isn't very DRY
+        var success = true;
+        if(!validateEmail(formData.email)) {success=false;}
+        if(!validateUsername(formData.username)){success=false};
+        if(!validatePass(formData.password, confPassword)){success=false;}
+
+        if(!success){
+            return
+        }
+
+        var hash = bcrypt.hashSync(formData.password, salt);
+        formData.password = hash;
+
+        console.log(formData, confPassword);
+
+        try{
+            const accountResponse = await sendAPIRequest({
+                requestType: "registerAccount", 
+                username: formData.username, 
+                email: formData.email, 
+                password: formData.password}, 
+                getOriginalServerUrl()); 
+        } catch (e){
+            setErrorMessage("Information is already registered to an account");
+            return;
+        }
+        //<Redirect to="/Board" />
+    };
+
     return(
         <>
-        {success ? (
-            <div>
-                <h2>Signup Successful!</h2>
-                {/*DOM router to take us to home page*/}
-            </div>
-        ) : (
+            <Header/>
             <div className='auth-wrapper'>
-                <p ref={errorRef} className={errorMessage ? "signinError" : "offscreen"}>
+                <p className={errorMessage ? "signinError" : "offscreen"}>
                     {errorMessage}
                 </p>
-                <h2>Sign In</h2>
+                <h2>Sign Up</h2>
                 <form onSubmit={handleSignin}>
                     <div className='input-field'>
                         <label>
@@ -77,7 +115,7 @@ const Signup = () => {
                                 name='username'
                                 placeholder='Enter username'
                                 onChange={handleChange}
-                                required/>
+                                />
                         </label>
                     </div>
 
@@ -92,6 +130,17 @@ const Signup = () => {
                                 required/>
                         </label>
                     </div>
+                    <div className='input-field'>
+                        <label>
+                            Confirm Password:
+                            <input 
+                                type='password' 
+                                name='confPassword'
+                                placeholder='Re-enter password'
+                                onChange={handlePWChange}
+                                required/>
+                        </label>
+                    </div>
 
                     <button
                         className='submit-button'
@@ -100,10 +149,9 @@ const Signup = () => {
                     </button>
                 </form>
                 <br></br>
-                <p className='signup-subscript'>Already have an account? <a className='subscript-link' href='#'>Click here to log in.</a>
+                <p className='signup-subscript'>Already have an account? {/*<Link to="/Login" className='subscript-link'>Click here to log in.</Link>*/}
                 </p>
             </div>
-        )}
         </>
     )
 }
