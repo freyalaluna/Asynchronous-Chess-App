238e24d69f46dbc8924160b76475e86fb0cd2d7b
Victor Berggren <victorberggren3@gmail.com>
2022-11-01 13:03:11 -0600

    didn't mean to include changes to Board.js

--- client/src/components/Gameplay/Board.js
+++ client/src/components/Gameplay/Board.js
@@ -2,21 +2,9 @@ import React from 'react';
 import { Chessboard } from 'react-chessboard';
 
 export default function Board(props) {
-  console.log("rendering chessboard");
   return (
     <div className="primary-board-container">
       <Chessboard id="BasicBoard" />
     </div>
   );
 }
-
-// function onDrop(sourceSquare, targetSquare, piece) {
-//   console.log("source square:");
-//   console.log(sourceSquare);
-//   console.log("targte square:");
-//   console.log(targetSquare);
-//   console.log("piece:");
-//   console.log(piece);
-
-//   return true;
-// }
