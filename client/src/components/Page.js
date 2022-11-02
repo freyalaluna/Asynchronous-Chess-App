0fbc8c9bc2b5a5b6d91591ff16b2fe40a1cf3466
Victor Berggren <victorberggren3@gmail.com>
2022-11-02 09:29:59 -0600

    added dynamic resizing for chessboard

--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -10,12 +10,24 @@ import { useAccount } from '../hooks/useAccount';
 export default function Page(props) {
 	// const [showAbout, toggleAbout] = useToggle(false);
 	//const { places, selectedIndex, placeActions } = usePlaces();
+	const [chessboardWidth, setChessboardWidth] = useState(400);
 	const {account, setAccount, userID, setUserID, email, setEmail, accountActions} = useAccount();
+
+	useEffect(() => {
+    function handleResize() {
+      const display = document.getElementsByClassName("primary-board-container")[0];
+      setChessboardWidth(display.offsetWidth - 20);
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
 	return (
 		<>
 			<Header />
-			<Board />
+			<Board boardWidth={chessboardWidth} />
 		</>
 	);
 }
