8eeb9ee770ec7a83e32292b23698b5d204fdec70
Rhea Chan <rheachan9983@gmail.com>
2022-11-05 22:27:24 -0600

    added testGetNotificationList and testGetInvitationList

--- server/src/test/java/com/tco/model/TestInbox.java
+++ server/src/test/java/com/tco/model/TestInbox.java
@@ -13,13 +13,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestInbox {
     private Inbox inbox;
     private Notification notif1;
+    private Notification notif2;
     private Invitation invite1;
+    private Invitation invite2;
     private Account sender;
     private Account receiver;
     private int matchID;
-    private String username = "Rhea9983";
-    private String email = "rhea@gmail.com";
-    private int accountID = 1;
+
+    // private String username = "Rhea9983";
+    // private String email = "rhea@gmail.com";
+    // private int accountID = 1;
 
     @BeforeEach
     public void initializeInbox() {
@@ -27,6 +30,24 @@ public class TestInbox {
         // sender = new Account();
         matchID = 12;
         notif1 = new Notification(MessageType.INVITE_PENDING, "Rhea Chan");
+        notif2 = new Notification(MessageType.INVITE_REJECTED, "Freya LaLuna");
         invite1 = new Invitation(sender, receiver, matchID);
+        invite2 = new Invitation(sender, receiver, matchID);
+    }
+    @Test
+    @DisplayName("rheachan9983: Test getNotificationList returns both notifications")
+    public void testgetNotificationList(){
+        inbox.allNotifs.add(notif1);
+        inbox.allNotifs.add(notif2);
+        inbox.getNotificationList();
+        assertEquals(inbox.allNotifs.size(), 2);
+    }
+    @Test
+    @DisplayName("rheachan9983: Test getInviteList returns both invites")
+    public void testgetInboxList(){
+        inbox.allInvites.add(invite1);
+        inbox.allInvites.add(invite2);
+        inbox.getInvitationList();
+        assertEquals(inbox.allInvites.size(), 2);
     }
 }
