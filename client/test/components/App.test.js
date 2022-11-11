bb7810b446318700730b8be27af09af02617e335
Victor Berggren <victorberggren3@gmail.com>
2022-11-11 14:46:30 -0700

    added test case for team and app name

--- client/test/components/App.test.js
+++ client/test/components/App.test.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { beforeEach, describe, test } from '@jest/globals';
-import { LOG } from '../../src/utils/constants';
+import { APP_NAME } from '../../src/utils/constants';
 import App from '../../src/components/App';
 
 describe('App', () => {
     beforeEach(() => {
         fetch.resetMocks();
+        render(<App />);
     });
 
-    test('victor45: render app', async () => {
-        render(<App />);
+    it('victor45: test team name in header', async () => {
+      await waitFor(() => {
+        const headings = screen.getByTestId('header-subtitle');
+        expect(headings.textContent).toEqual(CLIENT_TEAM_NAME);
+      })
+    });
+
+    it('victor45: test app name in header', async () => {
+      await waitFor(() => {
+        const headings = screen.getByTestId('header-title');
+        expect(headings.textContent).toEqual(APP_NAME);
+      })
     });
 
     test('mheavner: assert true', () => {
