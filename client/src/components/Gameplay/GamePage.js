30fea4f2dc0fa535e2c9606f7569bde749f89aab
Rhea Chan <rheachan9983@gmail.com>
2022-11-17 22:29:52 -0700

    added blackpawn png and shows up on page

--- client/src/components/Gameplay/GamePage.js
+++ client/src/components/Gameplay/GamePage.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import Board from '../Gameplay/Board';
 
+import blackPawn from '../../static/images/blackPawn.png';
+
 export default function GamePage(props) {
 
   return (
     <>
     <Board boardWidth={props.boardWidth}/>
     <div className='opponent-username'>Opponent</div>
+    <div className='player-captured-pieces'>
+      <img src={blackPawn}/>
+    </div>
     <div className='player-username'>Player</div>
     </>
   );
